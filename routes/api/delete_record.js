f94bc3f83f5c52c472d2568dd8c43642f337de2f
twalt007 <tatumwalter@gmail.com>
2019-12-22 21:38:34 -0800

    delete Api endpoint created

--- routes/api/delete_record.js
+++ routes/api/delete_record.js
@@ -1,11 +1,33 @@
 const db = require('../../db');
 
-module.exports = (req,res) => {
+module.exports = async (req,res) => {
     try{
         const {record_pid} = req.params;
 
+        let output = {
+            code: 404,
+            errors: [],
+            message: 'Bad DELETE Request',
+        }
 
-        res.send('I will be deleting something');
+        const [[record=null]] = await db.execute(`SELECT * FROM grades WHERE pid = ?`, [record_pid]);
+        if(!record){
+            output.errors.push(`No record found with an ID of: ${record_pid}`);
+            res.status(404).send({
+                ...output
+            });
+            return;
+        }
+
+        await db.execute(`DELETE FROM grades WHERE pid=?`,[record_pid]);
+        output.code = 200;
+        delete output.errors;
+        output.message = `Successfully deleted grade record: ${record_pid}`;
+        
+        res.status(output.code).send({
+            message: output.message,
+            deletedPid: record_pid
+        });
     }catch(err){
 
     }    
