187650dcefb6b4aad4a5b9e6288b30f8ba71a37d
twalt007 <tatumwalter@gmail.com>
2019-12-22 17:56:02 -0800

    added default error handling.

--- routes/api/get_grades.js
+++ routes/api/get_grades.js
@@ -1,12 +1,17 @@
 const db = require('../../db');
 
-module.exports = async(req,res) => {
-    const [results] = await db.query(`
+module.exports = async (req, res) => {
+    try {
+        const [results] = await db.query(`
         SELECT pid, course, grade, name, updated AS lastUpdated
         FROM grades
     `);
+
+        res.send({
+            records: results
+        })
+    }catch(err){
+        next(error);
+    }
     
-    res.send({
-        records: results
-    })
 };
